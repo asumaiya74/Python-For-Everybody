209dcff879f5f368cb45390d62666f4452b238b5
AYSHA SUMAIYA <asumaiya@umd.edu>
2023-12-16 20:16:06 -0500

    edited

--- Chapter 01/CH1_Exercises.py
+++ Chapter 01/CH1_Exercises.py
@@ -3,9 +3,9 @@ name and then welcomes them.
 Enter your name: Chuck
 Hello Chuck """
 
-'''
+
 en_name = input("Enter your name:")
-print("Hello", en_name)'''
+print("Hello", en_name)
 
 """ Exercise 3: Write a program to prompt the user for hours and rate per
 hour to compute gross pay. 
@@ -14,12 +14,12 @@ Enter Hours: 35
 Enter Rate: 2.75
 Pay: 96.25 """
 
-'''
+
 en_hrs = input("Enter Hours:")
 en_rate = input("Enter Rate:")
 pay = float(en_hrs) * float(en_rate)
 print("Pay: $", pay)
-'''
+
 
 '''Exercise 5: Write a program which prompts the user for a Celsius temperature, 
 convert the temperature to Fahrenheit, and print out the converted temperature.'''
