c6d1aa46b4ae28d7d7151fdfd3aad79cc61e8056
AYSHA SUMAIYA <asumaiya@umd.edu>
2024-02-03 19:09:34 -0500

    Looping through strings

---
+++ Chapter 06 - Strings/CH6_Exercises.py
@@ -0,0 +1,21 @@
+#Looking Inside Strings
+fruit = 'banana'
+letter = fruit[1]
+print(letter)
+
+x = 3
+w = fruit[x-1]
+print(w)
+
+#Strings having length
+print(len(fruit))
+
+#Looping Through Strings
+index = 0 
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
+
+for letter in fruit:
+    print(letter)
