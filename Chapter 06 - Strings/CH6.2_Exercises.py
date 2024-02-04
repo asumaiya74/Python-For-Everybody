c74da72fa68be9f4b754ec3678a378cb2a8abe89
AYSHA SUMAIYA <asumaiya@umd.edu>
2024-02-03 19:23:12 -0500

    #String Library ".lower()"

--- Chapter 06 - Strings/CH6.2_Exercises.py
+++ Chapter 06 - Strings/CH6.2_Exercises.py
@@ -5,3 +5,21 @@ fruit = 'banana'
 'nan' in fruit
 if 'a' in fruit:
     print("Found it!")
+
+#String Library ".lower()"
+#do not modify the original string but return a new string that has been altered 
+greet = 'Hello Bob'
+zap = greet.lower()
+print(zap) #makes it lower case letter
+print(greet) #Capitalizez first letter of each word
+print("Hi There". lower()) #makes it lower case letter
+
+#String Library ".upper()"
+nnn = greet.upper() #makes every letter uppercase 
+print(nnn)
+
+#Searching a string ".find()"
+pos = fruit.find('na')
+print(pos)
+aa = fruit.find('z')
+print(aa)
